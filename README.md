b7c22adf3af719855eb153a5fb58c93c36556189
edocico <138378767+edocico@users.noreply.github.com>
2023-10-05 17:05:58 +0200

    get dom element and eventlis.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # calcolo del prezzo del biglietto del treno
 ## raccolgo i dati nome e cognome dell'utente in una variabile tramite l'input nel DOM
+    - seleziono elemento tramite id 
+    - recupero il valore dell'input in una variabile
 ## raccolgo il dato km da percorrere in una variabile tramite l'input nel DOM
+    - seleziono elemento tramite id 
+    - recupero il valore dell'input in una variabile
 ## raccolgo il dato età utente in una variabile tramite la selection nel DOM
+    - seleziono elemento tramite id 
+    - recupero il valore dell'input in una variabile
 ## ascolto evento click sul bottone invia
--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 console.log('hello world!')
+alert('completa il form per ottenere il costo del tuo biglietto')
+
+const btnSendDOMElement = document.getElementById('send-btn')
+//# calcolo del prezzo del biglietto del treno
+//## raccolgo i dati nome e cognome dell'utente in una variabile tramite l'input nel DOM
+    //- seleziono elemento tramite id
+const usernameDOMElement = document.getElementById('username')
+console.dir(usernameDOMElement)
+    //- recupero il valore dell'input in una variabile
+
+//## raccolgo il dato km da percorrere in una variabile tramite l'input nel DOM
+    //- seleziono elemento tramite id
+const nKmDOMElement = document.getElementById('travelkm')
+console.dir(nKmDOMElement)
+    //- recupero il valore dell'input in una variabile
+
+//## raccolgo il dato età utente in una variabile tramite la selection nel DOM
+    //- seleziono elemento tramite id
+const userAgeDOMElement = document.getElementById('discount')
+console.dir(userAgeDOMElement)
+    //- recupero il valore dell'input in una variabile
+
+//## ascolto evento click sul bottone invia
+
+btnSendDOMElement.addEventListener('click', function (){
+
+    const userName = usernameDOMElement.value
+    console.log(userName)
+    const nKm = parseFloat(nKmDOMElement.value)
+    console.log(nKm)
+    const userAge = parseInt(userAgeDOMElement.value)
+    console.log(userAge)
+
+    let basePrice = nKm * 0.21
+    
+})
